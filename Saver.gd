e567d939a5303becaca6b26214da2c35d311df94
Eugene <e.karchevsky@mail.ru>
2022-02-06 13:50:22 +0400

    Sorting Saves by Date

--- Saver.gd
+++ Saver.gd
@@ -1,10 +1,10 @@
 extends Node
 
-var PathToFile
-
 var game_info = {
-	"level": "",
-	"x": "0",
+	"level": 0,
+	"x": 0,
+	
+	"datetime": 0
 	}
 var settings_info = {
 	"selectedlanguage" : "English",
@@ -12,14 +12,60 @@ var settings_info = {
 	}
 var language = {}
 
+var sortdates = []
+var sortfiles = []
+
 func Save_Game(Name = "Empty"):
+	game_info["datetime"] = OS.get_datetime()
 	var file = File.new()
 	file.open("res://Saves/" + Name + ".json", File.WRITE)
 	file.store_string(to_json(game_info))
 	file.close()
+
+func Clever_Load(PathToDir = "res://Saves/"):
+	var dirsave = Directory.new()
+	dirsave.open(PathToDir)
+	dirsave.list_dir_begin(true, false)
+	var file_name = dirsave.get_next()
+	var file =  File.new()
+	var dates = []
+	var files = []
+	while file_name != "":
+		if file_name.get_extension() == "json":
+			var PathToFile = "res://Saves/" + file_name
+			file.open(PathToFile, File.READ)
+			var date = (parse_json(file.get_as_text()))["datetime"]
+			dates.append(date)
+			files.append(file_name)
+			file.close()
+			file_name = dirsave.get_next()
+		else: file_name = dirsave.get_next()
+	
+	#Sorting by date
+	var order = {"year" : 31536000, "month" : 2419200, "day" : 80000, "hour" : 3600, "minute" : 60, "second" : 1}	#Iteration order and number of seconds
+	var DiS = dates.duplicate(true)		#Dates in second; deep = True
+	for i in DiS:
+		for n in order:
+			i[n] = i[n] * order.get(n)
+		DiS[DiS.find(i)] = i["year"] + i["month"] + i["day"] + i["hour"] + i["minute"] + i["second"]
+	var minc = 0	#Competition for much little number
+	while files.size() > 0:
+		for i in DiS:
+			if minc == 0 or i > minc:
+				minc = i
+		for i in DiS:
+			if i == minc:
+				sortdates.append(dates[DiS.find(i)])
+				sortfiles.append(files[DiS.find(i)])
+				dates.pop_at(DiS.find(i))
+				files.pop_at(DiS.find(i))
+				DiS.pop_at(DiS.find(i))
+				minc = 0
+			
+			
 func Load_Game(Name):
 	var file = File.new()
-	PathToFile = "res://Saves/" + Global.SelectedSave + ".json"
+	var PathToFile = "res://Saves/" + Global.SelectedSave + ".json"
 	file.open(PathToFile, File.READ)
 	var data = parse_json(file.get_as_text())
 	file.close()
@@ -27,12 +73,12 @@ func Load_Game(Name):
 		game_info = data
 func Load_Language():
 	var file = File.new()
-	PathToFile = ("res://Saves/Languages/" + settings_info["selectedlanguage"] + ".json")
+	var PathToFile = ("res://Saves/Languages/" + settings_info["selectedlanguage"] + ".json")
 	file.open(PathToFile, File.READ)
 	var data = parse_json(file.get_as_text())
 	file.close()
 	if typeof(data) == TYPE_DICTIONARY:
-		game_info = data
+		language = data
 			
 func convertering(path):
 	var file = File.new()
@@ -41,4 +87,3 @@ func convertering(path):
 	file.open(path.get_basename() + ".json", File.WRITE)
 	file.store_string(to_json(data))
 	file.close()
-	#erase
